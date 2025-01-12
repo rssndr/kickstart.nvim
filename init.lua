0c9b81dd8af2c4952ef5c878475e638e5ff9b497
a-rossetti <rossettiandrea276@gmail.com>
2025-01-12 14:58:45 +0100

    added github copilot

--- init.lua
+++ init.lua
@@ -919,6 +919,13 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
+  { -- Integrated Github Copilot
+    'github/copilot.vim',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    build = ':Copilot auth',
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
